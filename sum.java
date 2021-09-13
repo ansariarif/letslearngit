b9511a709966a56a5ba5c3dc092cc9ec25208a8e
arif1007 <ansariarif1007@gmail.com>
2021-09-13 06:30:35 +0000

    initial commit

---
+++ sum.java
@@ -0,0 +1,6 @@
+public class sum{
+     public static void main(String[] args)
+          int num1 = 5 , num2 = 10 , sum;
+          sum = sum1 + num2;
+          System.out.println("sum of these line" +sum);
+}
