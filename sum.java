156fa885edc87492bde06dc1ff9741d32ba0a7f4
arif1007 <ansariarif1007@gmail.com>
2021-09-13 06:54:05 +0000

    meaasage chnaged in sum.java

--- sum.java
+++ sum.java
@@ -1,5 +1,5 @@
 public class sum{
-     public static void main(String[] args)
+     public static void main(String[] args){
           int num1 = 5 , num2 = 10 , sum;
           sum = sum1 + num2;
           System.out.println("sum of these line" +sum);
